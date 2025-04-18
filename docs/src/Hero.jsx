7052c2e49e0bd957b952a0a8ba422f273b1f0e99
TheBritishBoyo <141440456+TheBritishBoyo@users.noreply.github.com>
2025-04-18 20:41:47 +0100

    (extras)

--- docs/src/Hero.jsx
+++ docs/src/Hero.jsx
@@ -2,10 +2,15 @@ import './Hero.css'
 
 function Hero() {
     return(
+
         <div className="hero">
+
             <h1>MODBUNKER</h1>
-            <p>» The safest most high quality mods. «</p>
+            <p>» The safest most high quality mods «</p>
+            <br className="top-linebreak"></br>
+            
         </div>
+
     );
 }
 
