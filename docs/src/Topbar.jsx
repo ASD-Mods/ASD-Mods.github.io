a2761690b93fabb45a487d6a8c9784228d609d24
ItsGnome <lucacurtisfoster@icloud.com>
2025-04-18 21:31:48 +0100

    make links

--- docs/src/Topbar.jsx
+++ docs/src/Topbar.jsx
@@ -8,6 +8,14 @@ function Topbar (){
                 <div className="logo-circle">LOGO</div>
                 <span className="brand">MODBUNKER</span>
             </div>
+            <div className="topbar-middle">
+                <div className='container'>
+                    <a className='topbar-link active' href="#Home">Home</a>
+                    <a className='topbar-link' href="#Games">Games</a>
+                    <a className='topbar-link' href="#Modders">Modders</a>
+                    <a className='topbar-link' href="#Help">Help</a>
+                </div>
+            </div>
             <div className="topbar-right">
                 <div className="menu-icon">☰</div>
             </div>
