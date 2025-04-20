c51b99e79f2902ac8cb89a1c5fd720cda8064843
ItsGnome <lucacurtisfoster@icloud.com>
2025-04-21 00:59:56 +0100

    Update SideBySidePara.jsx

--- docs/src/components/SideBySidePara.jsx
+++ docs/src/components/SideBySidePara.jsx
@@ -1,21 +1,24 @@
-import React from 'react'
-import "./SideBySidePara.css"
-import tempImage from '../assets/capsule_616x353.jpg'
+import React, { ReactNode } from "react";
+import "./SideBySidePara.css";
+import tempImage from "../assets/capsule_616x353.jpg";
 
-/* MAKE THIS REUSABLE AND ADD PROPS SO IT WORKS */
+/* make image a property */
 
-const SideBySidePara = () => {
+
+const SideBySidePara = (Props) => {
   return (
-    <div className='SideBySidePara'>
-        <div className='SBSP-Left'>
-            <h3>Many different games that you can chose from.</h3>
-            <p>Choose from over 23048702537 different games for you to play, mod, and enjoy.</p>
-        </div>
-        <div className='SBSP-Right'>
-            <img width="100%" src={tempImage} alt="STUFF" />
-        </div>
+    <div className="SideBySidePara">
+      <div className="SBSP-Left">
+        <h3>{Props.Title}</h3>
+        <p>
+          {Props.children}
+        </p>
+      </div>
+      <div className="SBSP-Right">
+        <img width="100%" src={tempImage} alt="STUFF" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SideBySidePara
+export default SideBySidePara;
