34957238aa72bbaf1ee6b7663c2efebbc87425a7
ItsGnome <lucacurtisfoster@icloud.com>
2025-04-23 20:27:21 +0100

    Update Footer.jsx

--- docs/src/components/Footer.jsx
+++ docs/src/components/Footer.jsx
@@ -15,34 +15,25 @@ const Footer = () => {
 
         <div className='footer-col socials'>
           <h3>About</h3>
-          <a href="">Discord</a>
-          <a href="">Twitter</a>
-          <a href="">Youtube</a>
-          <a href="">Instagram</a>
+          <a href="">Blog</a>
+          <a href="">Changelog</a>
+          <a href="">Status</a>
+          <a href="">Careers</a>
+          <a href="">Rewards Program</a>
         </div>
         
         <div className='footer-col socials'>
           <h3>Products</h3>
-          <a href="">Discord</a>
-          <a href="">Twitter</a>
-          <a href="">Youtube</a>
-          <a href="">Instagram</a>
         </div>
 
         <div className='footer-col socials'>
           <h3>Resources</h3>
-          <a href="">Discord</a>
-          <a href="">Twitter</a>
-          <a href="">Youtube</a>
+          <a href="">Instagram</a>
           <a href="">Instagram</a>
         </div>
 
         <div className='footer-col socials'>
           <h3>Legal</h3>
-          <a href="">Discord</a>
-          <a href="">Twitter</a>
-          <a href="">Youtube</a>
-          <a href="">Instagram</a>
         </div>
       </div>
     </div>
