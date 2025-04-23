762919c87429417c180c51af78bf72a916bc8872
ItsGnome <lucacurtisfoster@icloud.com>
2025-04-23 23:31:29 +0100

    start on the games page

--- docs/src/pages/Games.jsx
+++ docs/src/pages/Games.jsx
@@ -1,52 +1,37 @@
-import Topbar from '../components/Topbar.jsx'
-import Customlinebreak from '../components/CustomLineBreak.jsx'
+import GameCard from '../components/GameCard.jsx'
 import '../index.css'
 import './Games.css'
 
 const Home = () => {
   return (
     <>
-      <div className="hero">
 
-            <h1>GAMES PAGE</h1>
-            <p>» THIS IS TEMP «</p>
-            
-        </div>
-        
-      <Customlinebreak />
-      <Customlinebreak />
+    <div>
+      <div className='searchbar'>
+        seach for games
+      </div>
+      
+      <div className='filters'>
+        filters which games are shown (only show adventure etc)
+      </div>
+      
+      <div className='sort-order'>
+        order that the games sorted in (most popular first etc)
+      </div>
+    </div>
 
-      <p className="wesbite-info">
-        <strong>Over 50,000 mods</strong> downloaded worldwide, trusted by <strong>thousands of players</strong> every day.<br />
-        From <strong>magical biomes</strong> and <strong>custom dimensions</strong> to <strong>realistic vehicles</strong> and <strong>epic boss fights</strong>,<br />
-        our content pushes the boundaries of what’s possible in your game.<br />
-        Explore, download, and transform your gameplay with the <strong>highest quality mods</strong> on the internet.
-        </p>
 
-      <Customlinebreak />
-      <Customlinebreak />
-      <Customlinebreak />
-      <Customlinebreak />
-      <Customlinebreak />
-      <Customlinebreak />
-      <Customlinebreak />
-      <Customlinebreak />
-      <Customlinebreak />
-      <Customlinebreak />
-      <Customlinebreak />
-      <Customlinebreak />
-      <Customlinebreak />
-      <Customlinebreak />
-      <Customlinebreak />
-      <Customlinebreak />
-      <Customlinebreak />
-      <Customlinebreak />
-      <Customlinebreak />
-      <Customlinebreak />
-      <Customlinebreak />
-      <Customlinebreak />
-      <Customlinebreak />
-      <Customlinebreak />
+    <div className="games-grid">
+        <GameCard /> <GameCard /> <GameCard /> <GameCard /> <GameCard />
+        <GameCard /> <GameCard /> <GameCard /> <GameCard /> <GameCard />
+        <GameCard /> <GameCard /> <GameCard /> <GameCard /> <GameCard />
+        <GameCard /> <GameCard /> <GameCard /> <GameCard /> <GameCard />
+    </div>
+    
+
+    <div className='page-nav'>
+      ADD PAGE NAV
+    </div>
 
     </>
   )
