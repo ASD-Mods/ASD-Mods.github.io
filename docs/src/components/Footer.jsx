eb205495a22543bd458e94fa73a5948c41d05c53
TheBritishBoyo <141440456+TheBritishBoyo@users.noreply.github.com>
2025-04-25 13:18:39 +0100

    Improved Footer + removed clutter code

--- docs/src/components/Footer.jsx
+++ docs/src/components/Footer.jsx
@@ -5,7 +5,8 @@ const Footer = () => {
   return (
     <div className='footer-div'>
       <div className='footer-row'>
-        <div className='footer-col socials'>
+
+        <div className='footer-col'>
           <h3>Socials</h3>
           <a href="">Discord</a>
           <a href="">Twitter</a>
@@ -13,7 +14,7 @@ const Footer = () => {
           <a href="">Instagram</a>
         </div>
 
-        <div className='footer-col socials'>
+        <div className='footer-col'>
           <h3>About</h3>
           <a href="">Blog</a>
           <a href="">Changelog</a>
@@ -21,20 +22,19 @@ const Footer = () => {
           <a href="">Careers</a>
           <a href="">Rewards Program</a>
         </div>
-        
-        <div className='footer-col socials'>
-          <h3>Products</h3>
-        </div>
 
-        <div className='footer-col socials'>
+        <div className='footer-col'>
           <h3>Resources</h3>
           <a href="">Ticket Center</a>
           <a href="">Report Issues</a>
         </div>
 
-        <div className='footer-col socials'>
+        <div className='footer-col'>
           <h3>Legal</h3>
+          <a href="">Terms of Service</a>
+          <a href="">Privacy Policy</a>
         </div>
+
       </div>
     </div>
   )
